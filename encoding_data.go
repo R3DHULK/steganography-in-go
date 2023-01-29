f263d55320a6a3844ee636aed4a05d71637ffd23
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2023-01-29 21:41:18 +0530

    Tool To Use Encoding

--- encoding_data.go
+++ encoding_data.go
@@ -11,7 +11,7 @@ func main(){
 	// 0x22 0x33 .....0x45 0x44 0xff
 	
 	carrierFile := "hulk.jpg"   // File Name Where You Use To Hide
-	MaliciousFile := "dns-go.png"    // File Name What You Use To Hide
+	MaliciousFile := "captain.jpg"    // File Name What You Use To Hide
 	encodedFile := "encoded_hulk.jpg"   // Result
 	
 	err := steg.EncodeByFileNames(carrierFile, MaliciousFile, encodedFile)
